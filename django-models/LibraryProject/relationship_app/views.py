41c11634f1f0a9c2a5a682588072cf0a25eb1027
Richmond Fosu <fosuserebour@gmail.com>
2025-11-10 19:00:01 +0000

    midify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Author, Book, Library, Librarian
+from .models import Library, Book
 from django.views.generic import DetailView, ListView
 
 # Create your views here.
