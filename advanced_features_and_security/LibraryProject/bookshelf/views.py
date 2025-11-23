df529a58f09c6e280e4a5040395ac0e455610f3f
Richmond Fosu <fosuserebour@gmail.com>
2025-11-23 21:19:17 +0000

    modify bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from django.contrib.auth.decorators import permission_required
 from django import forms
-
+from .forms import ExampleForm
 # Create your views here.
 
 @permission_required('bookshelf.can_view_book', raise_exception=True)
