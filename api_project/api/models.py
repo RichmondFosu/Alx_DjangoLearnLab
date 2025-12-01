b26045fb65332a88ee970e37df9bb68c55f6be3d
Richmond Fosu <fosuserebour@gmail.com>
2025-12-01 17:10:20 +0000

    add book serializer

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    
+
+
+    def __str__():
+        self.title
